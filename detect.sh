69fa73b01a9426c8d53277080e7a9e02beb560ae
Remco de Man <gitlab@remcodeman.nl>
2017-09-04 20:01:28 +0200

    Make detection of Arduino more foolproof by using find instead of ls.

--- detect.sh
+++ detect.sh
@@ -3,11 +3,11 @@
 read -p "Unplug the Arduino when plugged in. Then press enter."
 
 OUTPUT_FILE="$(mktemp)"
-ls -d /dev/* > "$OUTPUT_FILE"
+find /dev/ -maxdepth 1 -not -type d | sort > "$OUTPUT_FILE"
 
 read -p "Plug in the Arduino now. Then press enter to continue."
 
 echo "Possible devices for the Arduino are:"
-comm -1 -3 "$OUTPUT_FILE" <(ls -d /dev/*)
+comm -1 -3 "$OUTPUT_FILE" <(find /dev/ -maxdepth 1 -not -type d | sort)
 rm -f "$OUTPUT_FILE"
 exit 0
