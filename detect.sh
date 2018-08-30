773d8e8ed62eb84736a89fbe64c9241e733170a6
Remco de Man <gitlab@remcodeman.nl>
2018-08-30 21:17:46 +0200

    Make detect script compatible with macOS, porting all the tools.

--- detect.sh
+++ detect.sh
@@ -3,11 +3,11 @@
 read -p "Unplug the Arduino when plugged in. Then press enter."
 
 OUTPUT_FILE="$(mktemp)"
-find /dev/ -maxdepth 1 -not -type d | sort > "$OUTPUT_FILE"
+find /dev -maxdepth 1 -not -type d | sort > "$OUTPUT_FILE"
 
 read -p "Plug in the Arduino now. Then press enter to continue."
 
 echo "Possible devices for the Arduino are:"
-comm -1 -3 "$OUTPUT_FILE" <(find /dev/ -maxdepth 1 -not -type d | sort)
+comm -1 -3 "$OUTPUT_FILE" <(find /dev -maxdepth 1 -not -type d | sort)
 rm -f "$OUTPUT_FILE"
 exit 0
